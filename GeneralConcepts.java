86fa07a6a2f86be77561ca4ce2dd3e39e01f0afd
rinchman <rinchen4433@gmail.com>
2018-09-26 00:44:40 -0500

    added GeneralConcepts class

---
+++ GeneralConcepts.java
@@ -0,0 +1,23 @@
+public class GeneralConcepts {
+// Java is platform independent. That is made possible by JVM.
+
+// JVM lives inside JRE (Java Runtime Environment). JRE is included in JDK (Java Development Kit). JDK also includes compilers, debuggers, and tools to create programs. 
+
+/* Class vs Instances
+    ~ Classes are the blueprints for the objects we want to create.
+    ~ From the blueprint, we can create different types of the object which are instances of that class.
+*/
+
+/* Static vs Nonstatic Methods
+    ~ Static methods are class methods
+    ~ Static methods are called using class name if caller is outside the current class
+    ~ Static methods can only access static/class variables
+
+    ~ Nonstatic methods are instance methods
+    ~ Nonstatic methods are called using instance name
+    ~ Nonstatic methods can access static and nonstatic variables
+*/
+
+
+
+}
